98ca29f60b8be7dd4457175ae822ad088aec781b
kouadir <chaymae.kouadir@gmail.com>
2017-05-04 21:53:03 +0200

    add pokemonfactory

--- src/main/java/fr/univavignon/pokedex/imp/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/imp/PokemonFactory.java
@@ -1,5 +1,17 @@
 package fr.univavignon.pokedex.imp;
 
-public class PokemonFactory {
+import java.io.Serializable;
 
+import fr.univavignon.pokedex.api.*;
+
+public class PokemonFactory implements IPokemonFactory, Serializable {
+
+	@Override
+	public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
 }
