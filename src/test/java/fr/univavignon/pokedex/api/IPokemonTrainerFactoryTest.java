d1d89aa66fd6ee37c6299a4ef9905f0f7beece10
kouadir <chaymae.kouadir@gmail.com>
2017-05-08 04:20:22 +0200

    add ipokemontrainerfactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -1,6 +1,5 @@
 package fr.univavignon.pokedex.api;
 
-import static org.junit.Assert.*;
 import org.junit.*;
 import org.mockito.*;
 
@@ -8,7 +7,26 @@ import org.mockito.*;
 public class IPokemonTrainerFactoryTest {
 
 
-
+	@Mock
+	protected IPokemonTrainerFactory ptF;
+	
+	@Mock
+	protected IPokedexFactory pF;
+	
+	@Mock
+	private IPokedex p;
+	private Pokemon pok = new Pokemon(0, "Bulbasaur", 126, 126, 90, 613, 64, 4000, 4, 56);
+	
+	@Before
+	public void setUp() throws PokedexException {
+			PokemonTrainer pT = new PokemonTrainer("trainer1", Team.INSTINCT, p);
+			Mockito.when(ptF.createTrainer("trainer1", Team.INSTINCT, pF)).thenReturn(pT);
+			Mockito.when(p.size()).thenReturn(1);
+			Mockito.when(p.addPokemon(pok)).thenReturn(0);
+			Mockito.when(p.getPokemon(0)).thenReturn(pok);
+		
+		
+	}
 	
     
 }
