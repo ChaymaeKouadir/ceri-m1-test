dcb58fea59be25f5f61efd8bdf8ad0f975bde481
kouadir <chaymae.kouadir@gmail.com>
2017-05-08 03:26:45 +0200

    update ipokemontrainerfactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonTrainerFactoryTest.java
@@ -7,21 +7,7 @@ import org.mockito.*;
 
 public class IPokemonTrainerFactoryTest {
 
-	@Mock private IPokemonTrainerFactory pokemonTrainerFactory;
-	private IPokedex p;
-	private IPokedexFactory pF;
-	private PokemonTrainer pT = new PokemonTrainer("Trainer", Team.MYSTIC,  p);
 
-	@Before
-	public void setUp() throws PokedexException {
-		MockitoAnnotations.initMocks(this);
-		Mockito.when(pokemonTrainerFactory.createTrainer("Trainer", Team.MYSTIC,  pF)).thenReturn(pT);
-	}
-	
-	@Test
-	public void createTrainerTest() {
-		assertEquals(pT, pokemonTrainerFactory.createTrainer("Trainer", Team.MYSTIC,  pF));
-	}
 
 	
     
